743126c704e5c8180900ce8bbe9e53035eb23748
dkssyddico <dkssyddico@naver.com>
2023-01-12 22:52:54 +0900

    feat: add user when making message book Next auth에선 server side에서 unstable_getServerSession을 하라고 했는데 experimental api라며 오류가 남 그래서 getSession을 사용하긴 했는데개발이 덜 되었는지 공식문서에서도 설명이 부족한 거 같다

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,9 +22,9 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
   ],
-  // session: {
-  //   strategy: 'database',
-  //   maxAge: 30 * 24 * 60 * 60, // 30 days
-  //   updateAge: 24 * 60 * 60, // 24 hours
-  // },
+  session: {
+    strategy: 'database',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
 });
--- pages/api/books/index.tsx
+++ pages/api/books/index.tsx
@@ -1,11 +1,7 @@
 import withHandler, { ResponseType } from '@libs/client/withHandler';
 import client from '@libs/server/client';
 import { NextApiRequest, NextApiResponse } from 'next';
-
-interface Question {
-  content: string;
-  required: boolean;
-}
+import { getSession } from 'next-auth/react';
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   if (req.method === 'GET') {
@@ -14,6 +10,10 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
   }
 
   if (req.method === 'POST') {
+    const session = await getSession({ req });
+    if (!session) {
+      return res.status(401).send({ success: false, message: 'Please log in' });
+    }
     const {
       body: { thumbnail, title, startDate, endDate, description, questions, hashtags },
     } = req;
@@ -43,6 +43,11 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
               create: { name: tag },
               where: { name: tag },
             })),
+          },
+          user: {
+            connect: {
+              email: session.user?.email!,
+            },
           },
         },
       });
