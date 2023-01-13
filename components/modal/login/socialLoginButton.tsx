93d69fdd5d44bb4e6e277e8e6aaf00ca422c01b2
dkssyddico <dkssyddico@naver.com>
2023-01-13 16:39:24 +0900

    style: change social login button bg-provider로 소셜 로그인마다 배경색을 줬는데 배경색이 나왔다 안나왔다해서 코드 스타일을 바꿈

--- components/modal/login/socialLoginButton.tsx
+++ components/modal/login/socialLoginButton.tsx
@@ -5,14 +5,32 @@ interface SocialLoginButtonProps {
 }
 
 export default function SocialLoginButton({ provider }: SocialLoginButtonProps) {
-  return (
-    <button
-      onClick={() => signIn(provider)}
-      className={`flex w-full items-center justify-center rounded-xl bg-${provider} p-2`}
-    >
-      <span className='font-semibold'>
-        {provider === 'naver' ? '네이버' : provider === 'kakao' ? '카카오' : '구글'} 로그인
-      </span>
-    </button>
-  );
+  if (provider === 'kakao') {
+    return (
+      <button
+        onClick={() => signIn(provider)}
+        className={`flex w-full items-center justify-center rounded-xl bg-kakao p-2`}
+      >
+        <span className='font-semibold'>카카오 로그인</span>
+      </button>
+    );
+  } else if (provider === 'naver') {
+    return (
+      <button
+        onClick={() => signIn(provider)}
+        className={`flex w-full items-center justify-center rounded-xl bg-naver p-2`}
+      >
+        <span className='font-semibold'>네이버 로그인</span>
+      </button>
+    );
+  } else {
+    return (
+      <button
+        onClick={() => signIn(provider)}
+        className={`flex w-full items-center justify-center rounded-xl bg-google p-2`}
+      >
+        <span className='font-semibold'>구글 로그인</span>
+      </button>
+    );
+  }
 }
