b0d9ecc8a850243818a4a5cbb20bc7928ba8da61
dkssyddico <dkssyddico@naver.com>
2023-02-12 23:39:52 +0900

    add todo

--- pages/my.tsx
+++ pages/my.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 
-// TODO: 내가 참여한 메세지북 내가 만든 메세지북.. 
+// TODO: 내가 참여한 메세지북 내가 만든 메세지북..
+// TODO: 내가 쓴 댓글..
 const MyPage: NextPage = () => {
   return <div>my</div>;
 };
