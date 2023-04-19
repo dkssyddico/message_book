d43dc0ba6b99b1c1de60ca359f0a52a259ae54dd
dkssyddico <dkssyddico@naver.com>
2023-04-19 18:45:35 +0900

    fix: change folder name from form to container

--- pages/books/new.tsx
+++ pages/books/new.tsx
@@ -19,12 +19,12 @@ import {
 } from 'state/form';
 import imageUpload from '@libs/client/imageUpload';
 import SubmitButton from '@components/common/submitButton';
-import DropContainer from '@components/form/dropContainer';
+import DropContainer from '@components/container/dropContainer';
 import InputBox from '@components/new/inputBox';
-import ThumbnailContainer from '@components/form/thumbnailContainer';
-import DateContainer from '@components/form/dateContainer';
-import QuestionsContainer from '@components/form/questionsContainer';
-import HashtagContainer from '@components/form/hashtagContainer';
+import ThumbnailContainer from '@components/container/thumbnailContainer';
+import DateContainer from '@components/container/dateContainer';
+import QuestionsContainer from '@components/container/questionsContainer';
+import HashtagContainer from '@components/container/hashtagContainer';
 
 interface BookForm {
   title: string;
